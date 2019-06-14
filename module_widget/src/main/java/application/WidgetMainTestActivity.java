0664376e6d0f48af3d63e84489f0527c418903ec
lvyeRose <7758258aA>
2019-06-14 18:35:07 +0800

    修复widget测试类

--- module_widget/src/main/java/application/WidgetMainTestActivity.java
+++ module_widget/src/main/java/application/WidgetMainTestActivity.java
@@ -1,11 +1,15 @@
 package application;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.lvyerose.recordmoney.lib_base.base.BaseActivity;
+import com.lvyerose.recordmoney.lib_widget.menu_add.MenuAddView;
 import com.lvyerose.recordmoney.module_widget.R;
 
 public class WidgetMainTestActivity extends BaseActivity {
+    MenuAddView menuAddView;
+
     @Override
     protected int setContentLayout() {
         return R.layout.module_widget_activity_main_test_layout;
@@ -13,6 +17,14 @@ public class WidgetMainTestActivity extends BaseActivity {
 
     @Override
     protected void startAction(Bundle savedInstanceState) {
+        menuAddView = findViewById(R.id.test_menu_add_view);
+    }
 
+    public void showTest(View view) {
+        menuAddView.setShow(true);
+    }
+
+    public void hintTest(View view) {
+        menuAddView.setShow(false);
     }
 }
